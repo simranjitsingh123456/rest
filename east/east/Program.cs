5ed4ead10725ba74c534823e4ec95e9cfcb5cf35
Simranjit Singh <30011264@student.toiohomai.ac.nz>
2018-09-26 14:16:28 +1200

    msg

---
+++ east/east/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace east
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter an number:");
+            int num1 = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= num1; i++)
+            {
+                Console.WriteLine($"{num1}");
+            }
+            Console.ReadLine();
+            int J = 1;
+            while (J <= 10)
+            {
+                Console.WriteLine($"{num1}*{J}={num1}*j");
+                J++;
+            }
+            int K = 1;
+        }
+    }
+}
